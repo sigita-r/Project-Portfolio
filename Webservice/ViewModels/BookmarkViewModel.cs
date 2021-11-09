d1bb6346fd610bacf85c2925c3bf3aab9063bcb0
Mads Jönsson <madsjonsson1997@gmail.com>
2021-11-09 11:44:49 +0100

    added a few viewmodels

---
+++ Webservice/ViewModels/BookmarkViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webservice.ViewModels
+{
+    public class BookmarkViewModel
+    {
+        public string Username { get; set; }
+        public string Note { get; set; }
+
+
+    }
+}
---
+++ Webservice/ViewModels/SearchHistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webservice.ViewModels
+{
+    public class SearchHistoryViewModel
+    {
+        public string Username { get; set; }
+        public string Query { get; set; }
+
+    }
+}
