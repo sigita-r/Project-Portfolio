20f7bc88a4f9f724f66582aacb5316078f685c75
Mads Jönsson <madsjonsson1997@gmail.com>
2021-11-09 18:08:32 +0100

    Still working on titleController

--- Webservice/Controllers/TitleController.cs
+++ Webservice/Controllers/TitleController.cs
@@ -29,7 +29,7 @@ namespace Webservice.Controllers
         [HttpGet]
         public IActionResult GetTitles()
         {
-            var titles = DataService.GetTitles();
+            var titles = _dataService.GetTitles();
 
             return Ok(titles.Select(x => GetTitleViewModel(x)));
         }
@@ -38,18 +38,7 @@ namespace Webservice.Controllers
         {
             return new TitleViewModel
             {
-                // Some IDs have a space encoded as %20. Here we remove the encoding from the URL
-                Url = (_linkGenerator.GetUriByName(HttpContext, nameof(GetTitle), new { title.Id })).Replace("%20", ""),
-                TitleType = title.TitleType,
-                PrimaryTitle = title.PrimaryTitle,
-                OriginalTitle = title.OriginalTitle,
-                IsAdult = title.IsAdult,
-                StartYear = title.StartYear,
-                EndYear = title.EndYear,
-                RunTimeMinutes = title.RunTimeMinutes,
-                Poster = title.Poster,
-                Awards = title.Awards,
-                Plot = title.Plot
+                
             };
         }
 
