36f8be2c15dea1d0185217e7eec17119b9faf5d6
Mads Jönsson <madsjonsson1997@gmail.com>
2021-11-09 12:04:28 +0100

    Added bookmarkVMs

--- Webservice/ViewModels/BookmarkTitleViewModel.cs
+++ Webservice/ViewModels/BookmarkTitleViewModel.cs
@@ -5,14 +5,11 @@ using System.Threading.Tasks;
 
 namespace Webservice.ViewModels
 {
-    public class BookmarkViewModel
+    public class BookmarkTitleViewModel
     {
         public string Username { get; set; }
 
         public string TitleName { get; set; }
         public string TitleNote { get; set; }
-
-        public string PersonalityName { get; set; }
-        public string PersonalityNote { get; set; }
     }
 }
