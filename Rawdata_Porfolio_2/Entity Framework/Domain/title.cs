a8c2d570553af0fe5d577ce51fd609da8052b217
Mads Jönsson <madsjonsson1997@gmail.com>
2021-11-16 18:41:47 +0100

    Made some of the title.cs properties nullable'
    Also did paging on titles

--- Rawdata_Porfolio_2/Entity Framework/Domain/title.cs
+++ Rawdata_Porfolio_2/Entity Framework/Domain/title.cs
@@ -8,13 +8,15 @@ namespace Rawdata_Porfolio_2.Pages.Entity_Framework.Domain
 {
     public class Title
     {
+
+        
         [Key]
         public int Id { get; set; }
         public string Type { get; set; }
         public bool IsAdult { get; set; }
-        public int Year_Start { get; set; }
-        public int Year_End { get; set; }
-        public int Runtime { get; set; }
+        public int? Year_Start { get; set; }
+        public int? Year_End { get; set; }
+        public int? Runtime { get; set; }
         public double ? AvgRating { get; set; }
         public string Poster { get; set; }
         public string Plot { get; set; }
