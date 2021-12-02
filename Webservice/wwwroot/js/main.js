696f119207c84bb9d81011579d96833c644c0f51
Patrik Zori <mrpatko0770@protonmail.com>
2021-12-02 18:32:21 +0100

    Built the wwwroot structure (jQuery, RequireJS, Knockout, Bootstrap)

---
+++ Webservice/wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: "lib/jquery/jquery-3.6.0",
+        knockout: "lib/knockout/knockout-3.5.1",
+        dataService: "services/dataService",
+        text: "lib/requirejs/text"
+    }
+});
