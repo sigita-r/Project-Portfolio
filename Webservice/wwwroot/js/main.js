ac7b3d7f7256c0c6af998e62881c1cd2e8bf1d42
Patrik Zori <mrpatko0770@protonmail.com>
2021-12-06 20:12:23 +0100

    Preparing the login and create account forms

--- Webservice/wwwroot/js/main.js
+++ Webservice/wwwroot/js/main.js
@@ -1,9 +1,21 @@
 ﻿require.config({
     baseUrl: 'js',
+    shim: {
+        bootstrap: {
+            deps: ["jquery"]
+        }
+    },
     paths: {
         jquery: "lib/jquery/jquery-3.6.0",
         knockout: "lib/knockout/knockout-3.5.1",
         dataService: "services/dataService",
-        text: "lib/requirejs/text"
+        text: "lib/requirejs/text",
+        bootstrap: "lib/bootstrap/bootstrap"
     }
+});
+
+require(["jquery", "bootstrap"], function($) {
+    $(function(){
+        $('#loginTriggerButton').show();
+    });
 });
